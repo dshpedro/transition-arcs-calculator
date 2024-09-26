d6411210ef6e9f87050cecbfdafb79124be682fa
dshpedro <94130906+dshpedro@users.noreply.github.com>
2024-09-26 18:13:51 -0300

    Fix function `calculate_arcs`

--- calculator.c
+++ calculator.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 List *calculate_arcs(Truth_table *tt) {
-    /* To gather the fundamentals to understand how this function works let's first 
-     * take a look at the truth table for 'a*b + !a*c':
+    /* To understand how this function works let's first take a look at 
+     * the truth table for 'a*b + !a*c':
      *
      * abc|S
      * 000|0        * 000
@@ -16,7 +16,7 @@ List *calculate_arcs(Truth_table *tt) {
      * 110|1
      * 111|1
      *
-     * Characteristics and patterns we can find:
+     * Characteristics and patterns we can observe:
      *    It has 3 variables, we can represent that with n = 3
      *    For simplicity's sake, let's refer to these as variable 0, 1 and 2:
      *    tt->variables[0] = 'a'
@@ -24,13 +24,12 @@ List *calculate_arcs(Truth_table *tt) {
      *    tt->variables[2] = 'c'
      *
      *    The lines with asterisks highlight 2 numbers with the hamming distance of 1.
-     *    These lines are the ones that can possibly harbor a transition arc depending 
-     *    on the result of each one, that is, if they also differ in their S value, then
-     *    they are considered a transition arc.
-     *
-     *    variables[0] has 1 occurrence of 4 lines with consecutives 0's
-     *    variables[1] has 2 occurrences of 2 lines with consecutives 0's
-     *    variables[2] has 4 coccurrences of 1 line with 'consecutive' 0
+     *    If they also differ in their S value they can be called a transition arc.
+     
+     *    Amount of consecutive lines with 0's
+     *    variables[0] has 4 lines with consecutive 0's
+     *    variables[1] has 2 lines with consecutive 0's
+     *    variables[2] has 1 line with 'consecutive' 0
      *
      *    We can infer the rule for consecutives lines with 0 as:
      *        2^(n-1-variable_index).
@@ -39,65 +38,30 @@ List *calculate_arcs(Truth_table *tt) {
      *        2^(2-1) = 2^1 = 2
      *        2^(2-2) = 2^0 = 1
      *
-     *    And the number of occurrences per variable_index is:
-     *        2^(variable_index)
-     *    e.g.:
-     *        2^0 = 1
-     *        2^1 = 2
-     *        2^2 = 4
-     *    
-     * To solve this problem we need to iterate trough every 0 and check it's
-     * Hamming pair of Hamming distance 1. Since the 0's aren't all lined up
-     * for most variables, we need a way to skip the 1's, for that we need a way
-     * to dinamically determine a limit apart from a `consecutives` variable.
-     *
-     * Upon examination of the truth table we can find the following limits:
-     *    variable 0: 4
-     *    variable 1: 2, 6
-     *    variable 2: 1, 3, 5, 7
-     *
-     * So the limit seems to start with the same value as the number of 
-     * consecutives 0's and then turn into:
-     *    limit = limit + 2^(consecutives)
-     *
-     * The start index of a new occurrence of consecutives 0's also has to be dinamic,
-     * we can find it by going back `consecutives` number of times from the limit.
-     *    start = limit - consecutives
-     *
-     * Since we know the position of the last consecutive 0 in each occurrence of a
-     * cycle, we can infer that the position below it will be the Hamming pair for 
-     * the new start
+     * This function will iterate trough each variable, focusing on the lines where
+     * it's value is 0, and compare it with it's pair of Hamming distance 1.
      */
     List *arcs = new_list();
     insert_at_end(arcs, "Transition Arcs:");
 
     // characteristics discussed at the top comment
-    int consecutives;                // number of consecutives 0's/1's
-    int hamming_pair;                // hamming distance of 1
-    int occurrences;                 // number of cycles with 0's followed by 1's
+    int consecutives;                // number of consecutives 0's
+    int first_hamming_pair;          // hamming distance of 1
     int n = tt->amount_of_variables; // n is also the result column
-    
-    // start and limit will change with each new occurrence of a cycle
-    int start;                       // starting row
-    int limit;                       // limit row
 
     // checks every possible arc for each variable
     for(int var_index = 0; var_index < n; var_index++) {
         consecutives = pow(2, (n-1) - var_index);
-        occurrences = pow(2, var_index);
+        first_hamming_pair = consecutives;
 
-        start = 0;
-        limit = consecutives;
-        hamming_pair = limit;
-
-        for(int i = 0; i < occurrences; i++) {
-            for(int row = start; row < limit; row++) {
-                if(tt->table[row][n] != tt->table[hamming_pair++][n])
+        int amount_of_rows = tt->amount_of_rows;
+        for(int row = 0; row < amount_of_rows; row++) {
+            if(tt->table[row][var_index] == 0) {
+                if(tt->table[row][n] != tt->table[first_hamming_pair + row][n])
                     insert_at_end(arcs, get_arc_string(tt, row, var_index));
             }
-            limit += pow(2, consecutives);
-            start = limit - consecutives;
-            hamming_pair = limit;
+            else
+                row += consecutives - 1;
         }
     }
   
